e9b26f46d0a73a482e496ecef752b3c33dd539c0
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-11-05 15:01:15 -0800

    migrations: add initial migration

--- migrations/0000_initialise.sql
+++ migrations/0000_initialise.sql
@@ -4,8 +4,8 @@ CREATE TABLE oauth (
     access_token_hash   TEXT    PRIMARY KEY,
     refresh_token       TEXT    NOT NULL,
     expires_at          INTEGER NOT NULL,
-    user                TEXT    NOT NULL,
-)
+    user                TEXT    NOT NULL
+);
 
 CREATE INDEX idx_oauth_expires_at ON oauth (expires_at);
 CREATE INDEX idx_oauth_user_expires_at ON oauth (user, expires_at);
